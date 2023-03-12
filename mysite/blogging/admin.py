80a6ee04226d87e3021731e53048b1eaf2525f2a
cmb225 <cmb225@uw.edu>
2023-03-11 20:50:32 -0800

    Added CategoryInline to blogging/admin.py.  Added code to include CategoryInline in PostAdmin, but ran into an error. It didn't like that I was using ManyToMany instead of a ForeignKey. Thankfully Stack Overflow sent me to the relevant section of Django's guide for their admin page. After reviewing that, I changed it to model = Category.posts.through. I also added the exclude = [posts] to CategoryAdmin. The admin page now functions as I'd hoped for, without any errors.

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
 class PostAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        CategoryInline,
+    ]
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    exclude = ["posts"]
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
