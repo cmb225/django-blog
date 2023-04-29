297010153e7f70995c079e904d3c4d38574a9286
cmb225 <cmb225@uw.edu>
2023-04-28 23:01:56 -0700

    Moved router.register for posts and categories to api/urls.py. API links now all appear together on one page.

--- api/urls.py
+++ api/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+from blogging import views as b_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'posts', b_views.PostViewSet)
+router.register(r'categories', b_views.CategoryViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
