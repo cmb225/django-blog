e8c235ee5ad4f92511e8bafcb8e868a821009c2e
cmb225 <cmb225@uw.edu>
2023-04-28 17:12:18 -0700

    Removed import of Group from api/serializers.py and api/views.py since I don't need that. Created blogging/serializers.py.

--- api/api/quickstart/views.py
+++ api/api/quickstart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from api.quickstart.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
